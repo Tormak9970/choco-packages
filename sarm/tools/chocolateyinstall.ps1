2a17ebc0c9f4dfa2d46508879b66452d860cc08f
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-07-21 20:54:27 -0500

    chore: added sarm checksum

--- sarm/tools/chocolateyinstall.ps1
+++ sarm/tools/chocolateyinstall.ps1
@@ -1,14 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Tormak9970/Steam-Art-Manager/releases/latest/download/steam-art-manager.msi'
-$url64      = ''
+$url        = 'https://github.com/Tormak9970/Steam-Art-Manager/releases/tag/v2.6.0/download/steam-art-manager.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'Steam Art Manager'
 
@@ -16,10 +14,8 @@ $packageArgs = @{
   # To determine checksums, you can get that from the original site if provided.
   # You can also use checksum.exe (choco install checksum) and use it
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
