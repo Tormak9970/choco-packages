57957c6f7c182b430ded6e13de090dff4619b561
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-07-21 20:55:32 -0500

    chore: removed comments

--- sarm/tools/chocolateyinstall.ps1
+++ sarm/tools/chocolateyinstall.ps1
@@ -10,12 +10,8 @@ $packageArgs = @{
 
   softwareName  = 'Steam Art Manager'
 
-  # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided.
-  # You can also use checksum.exe (choco install checksum) and use it
-  # e.g. checksum -t sha256 -f path\to\file
   checksum      = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
